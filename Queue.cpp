ecbdfd391fb2e952d7ac1a871f40e1e3d232e7e8
Nikita <nikita.keyn@yandex.ru>
2022-03-10 18:43:09 +0300

    added user menu

--- Queue.cpp
+++ Queue.cpp
@@ -8,10 +8,6 @@ Queue::Queue(int size) {
   this->size = size;
 }
 
-Queue::~Queue() {
-  delete[] queue;
-}
-
 Queue::Queue(const Queue &q) :
         queue(new Item[q.size]), size(q.size), end(q.end), head(q.head), count(q.count) {
   for (int i = 0; i < q.size; ++i) {
@@ -19,25 +15,19 @@ Queue::Queue(const Queue &q) :
   }
 }
 
-int Queue::getSize() const {
-  return size;
+Queue::~Queue() {
+  delete[] queue;
 }
 
-std::ostream &operator<<(std::ostream &out, const Queue &q) {
-  int *items = getQueueOfArray(q);
-  for (int i = 0; i < q.getCount(); ++i) {
-    out << items[i] << " ";
+int Queue::pop() {
+  if (count <= 0) {
+    cout << "Queue is empty\n";
+    return -1;
   }
-  out << "\n";
-  return out;
-}
-
-std::istream &operator>>(std::istream &in, Queue &q) {
-  int item;
-  cout << "Enter item>>";
-  in >> item;
-  q.push(item);
-  return in;
+  int item = queue[head].value;
+  head = (head + 1) % size;
+  count--;
+  return item;
 }
 
 void Queue::clear() {
@@ -56,8 +46,89 @@ void Queue::push(int item) {
   count++;
 }
 
+int Queue::getSize() const {
+  return size;
+}
+
+int Queue::getCount() const {
+  return count;
+}
+
+void Queue::fill_the_queue() {
+  static int r = 0;
+  srand(r++);
+  for (int i = 0; i < this->getSize(); ++i) {
+    this->push(rand() % 20 + 1 - 10);
+  }
+}
+
 Queue &Queue::operator++() {
   cin >> *this;
+  return *this;
+}
+
+Queue &Queue::operator--() {
+  this->pop();
+  return *this;
+}
+
+Queue &Queue::operator=(const Queue &q) {
+  if (this == &q) {
+    return *this;
+  }
+  queue = new Item[q.size];
+  size = q.size;
+  end = q.end;
+  head = q.head;
+  count = q.count;
+  for (int i = 0; i < q.size; ++i) {
+    queue[i].value = q.queue[i].value;
+  }
+  return *this;
+}
+
+Queue &Queue::operator+=(const Queue &q2) {
+  if (size != q2.getSize() || count != q2.getCount()) {
+    cout << "Incorrect sizes or count items" << endl;
+    throw new invalid_argument("Incorrect sizes or count items");
+  }
+  int *items1 = getQueueOfArray(*this);
+  int *items2 = getQueueOfArray(q2);
+
+  clear();
+  for (int i = 0; i < q2.getCount(); ++i) {
+    push(items1[i] + items2[i]);
+  }
+  return *this;
+}
+
+Queue &Queue::operator-=(const Queue &q2) {
+  if (size != q2.getSize() || count != q2.getCount()) {
+    cout << "Incorrect sizes or count items" << endl;
+    throw new invalid_argument("Incorrect sizes or count items");
+  }
+  int *items1 = getQueueOfArray(*this);
+  int *items2 = getQueueOfArray(q2);
+
+  clear();
+  for (int i = 0; i < q2.getCount(); ++i) {
+    push(items1[i] - items2[i]);
+  }
+  return *this;
+}
+
+Queue &Queue::operator/=(const Queue &q2) {
+  if (size != q2.getSize() || count != q2.getCount()) {
+    cout << "Incorrect sizes or count items" << endl;
+    throw new invalid_argument("Incorrect sizes or count items");
+  }
+  int *items1 = getQueueOfArray(*this);
+  int *items2 = getQueueOfArray(q2);
+
+  clear();
+  for (int i = 0; i < q2.getCount(); ++i) {
+    push(items1[i] / items2[i]);
+  }
   return *this;
 }
 
@@ -89,6 +160,22 @@ Queue operator--(Queue &q, int d) {
   return temp;
 }
 
+bool operator>(const Queue &q1, const Queue &q2) {
+  return q1.getCount() > q2.getCount();
+}
+
+bool operator<(const Queue &q1, const Queue &q2) {
+  return q1.getCount() < q2.getCount();
+}
+
+bool operator>=(const Queue &q1, const Queue &q2) {
+  return q1.getCount() >= q2.getCount();
+}
+
+bool operator<=(const Queue &q1, const Queue &q2) {
+  return q1.getCount() <= q2.getCount();
+}
+
 bool operator!=(const Queue &q1, const Queue &q2) {
   if (q1.getSize() != q2.getSize() || q1.getCount() != q2.getCount()) {
     return true;
@@ -105,52 +192,6 @@ bool operator!=(const Queue &q1, const Queue &q2) {
 
 bool operator==(const Queue &q1, const Queue &q2) {
   return !(q1 != q2);
-}
-
-bool operator>(const Queue &q1, const Queue &q2) {
-  return q1.getCount() > q2.getCount();
-}
-
-bool operator>=(const Queue &q1, const Queue &q2) {
-  return q1.getCount() >= q2.getCount();
-}
-
-bool operator<(const Queue &q1, const Queue &q2) {
-  return q1.getCount() < q2.getCount();
-}
-
-bool operator<=(const Queue &q1, const Queue &q2) {
-  return q1.getCount() <= q2.getCount();
-}
-
-Queue &Queue::operator=(const Queue &q) {
-  if (this == &q) {
-    return *this;
-  }
-  queue = new Item[q.size];
-  size = q.size;
-  end = q.end;
-  head = q.head;
-  count = q.count;
-  for (int i = 0; i < q.size; ++i) {
-    queue[i].value = q.queue[i].value;
-  }
-  return *this;
-}
-
-int *getQueueOfArray(const Queue &q1) {
-  int *items = new int[q1.count];
-  int pos = 0;
-  if (q1.end > q1.head) {
-    for (int i = q1.head; i < q1.end; i++)
-      items[pos++] = q1.queue[i].value;
-  } else if (q1.end <= q1.head && q1.count) {
-    for (int i = q1.head; i < q1.size; i++)
-      items[pos++] = q1.queue[i].value;
-    for (int i = 0; i < q1.end; i++)
-      items[pos++] = q1.queue[i].value;
-  } else cout << "Queue is empty\n";
-  return items;
 }
 
 Queue operator+(const Queue &q1, const Queue &q2) {
@@ -183,75 +224,34 @@ Queue operator/(const Queue &q1, const Queue &q2) {
   return q3;
 }
 
-Queue &Queue::operator+=(const Queue &q2) {
-  if (size != q2.getSize() || count != q2.getCount()) {
-    cout << "Incorrect sizes or count items" << endl;
-    throw new invalid_argument("Incorrect sizes or count items");
+std::ostream &operator<<(std::ostream &out, const Queue &q) {
+  int *items = getQueueOfArray(q);
+  for (int i = 0; i < q.getCount(); ++i) {
+    out << items[i] << " ";
   }
-  int *items1 = getQueueOfArray(*this);
-  int *items2 = getQueueOfArray(q2);
-
-  clear();
-  for (int i = 0; i < q2.getCount(); ++i) {
-    push(items1[i] + items2[i]);
-  }
-  return *this;
+  out << "\n";
+  return out;
 }
 
-Queue &Queue::operator/=(const Queue &q2) {
-  if (size != q2.getSize() || count != q2.getCount()) {
-    cout << "Incorrect sizes or count items" << endl;
-    throw new invalid_argument("Incorrect sizes or count items");
-  }
-  int *items1 = getQueueOfArray(*this);
-  int *items2 = getQueueOfArray(q2);
-
-  clear();
-  for (int i = 0; i < q2.getCount(); ++i) {
-    push(items1[i] / items2[i]);
-  }
-  return *this;
+std::istream &operator>>(std::istream &in, Queue &q) {
+  int item;
+  cout << "Enter item>>";
+  in >> item;
+  q.push(item);
+  return in;
 }
 
-Queue &Queue::operator-=(const Queue &q2) {
-  if (size != q2.getSize() || count != q2.getCount()) {
-    cout << "Incorrect sizes or count items" << endl;
-    throw new invalid_argument("Incorrect sizes or count items");
-  }
-  int *items1 = getQueueOfArray(*this);
-  int *items2 = getQueueOfArray(q2);
-
-  clear();
-  for (int i = 0; i < q2.getCount(); ++i) {
-    push(items1[i] - items2[i]);
-  }
-  return *this;
-}
-
-int Queue::getCount() const {
-  return count;
-}
-
-int Queue::pop() {
-  if (count <= 0) {
-    cout << "Queue is empty\n";
-    return -1;
-  }
-  int item = queue[head].value;
-  head = (head + 1) % size;
-  count--;
-  return item;
-}
-
-Queue &Queue::operator--() {
-  this->pop();
-  return *this;
-}
-
-void Queue::fill_the_queue() {
-  static int r = 0;
-  srand(r++);
-  for (int i = 0; i < this->getSize(); ++i) {
-    this->push(rand() % 20 + 1 - 10);
-  }
+int *getQueueOfArray(const Queue &q1) {
+  int *items = new int[q1.count];
+  int pos = 0;
+  if (q1.end > q1.head) {
+    for (int i = q1.head; i < q1.end; i++)
+      items[pos++] = q1.queue[i].value;
+  } else if (q1.end <= q1.head && q1.count) {
+    for (int i = q1.head; i < q1.size; i++)
+      items[pos++] = q1.queue[i].value;
+    for (int i = 0; i < q1.end; i++)
+      items[pos++] = q1.queue[i].value;
+  } else cout << "Queue is empty\n";
+  return items;
 }
